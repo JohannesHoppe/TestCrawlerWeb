99a2c3b95a4fdd3a996f0ba6c6f10f3137c639b7
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-15 09:29:12 +0200

    deployement

---
+++ CrawlerWeb/server.js
@@ -0,0 +1,24 @@
+﻿var connect = require('connect');
+var http = require('http');
+var port = process.env.port || 1337;
+var serveStatic = require('serve-static');
+
+
+var connect = require('connect');
+var http = require('http');
+
+var app = connect();
+
+
+app.use(serveStatic('public'));
+
+// respond to all requests
+
+app.use(function(req, res) {
+    res.end('Hello from Connect!\n');
+});
+
+
+//create node.js http server and listen on port
+http.createServer(app).listen(port);
+
